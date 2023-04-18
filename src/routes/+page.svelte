38053ab144cb8fdc5853e600b5ceb4974af8f1cc
Felix Green <work@felix.green>
2023-04-18 13:58:26 -0400

    replace markdown, add copy from doc

---
+++ src/routes/+page.svelte
@@ -0,0 +1,171 @@
+<script lang="ts">
+	import { Box, Span } from '@/lib/atoms'
+	import { Content, Stack, Inline, Text, Break, Grid } from '@/lib/bonds'
+</script>
+
+<Stack>
+	<!-- 1 -->
+
+	<Box
+		css={{
+			background: `linear-gradient(
+				to right,
+				$grey-50 0%,
+				$grey-50 48%,
+				$grey-950 52%,
+				$grey-950 100%
+			)`,
+		}}
+	>
+		<Content>
+			<Inline
+				css={{
+					minHeight: '100vh',
+					'> div': {
+						flex: 1,
+					},
+				}}
+			>
+				<Stack
+					align="center"
+					alignV="center"
+					css={{
+						flex: 1,
+					}}
+				>
+					<Text look="heading">Parable</Text>
+
+					<Text look="body">Do more with AI</Text>
+				</Stack>
+
+				<Stack
+					align="center"
+					alignV="center"
+					css={{
+						color: '$grey-50',
+						flex: 1,
+					}}
+				>
+					<Text look="heading">Thing Forge</Text>
+
+					<Text look="body">What do you want to make?</Text>
+				</Stack>
+			</Inline>
+		</Content>
+	</Box>
+
+	<!-- 2 -->
+
+	<Box
+		css={{
+			background: `linear-gradient(
+				to left,
+				$grey-50 0%,
+				$grey-50 48%,
+				$grey-950 52%,
+				$grey-950 100%
+			)`,
+		}}
+	>
+		<Content>
+			<Inline
+				css={{
+					minHeight: '100vh',
+					'> div': {
+						flex: 1,
+					},
+				}}
+			>
+				<Stack
+					align="center"
+					alignV="center"
+					css={{
+						color: '$grey-50',
+						flex: 1,
+					}}
+				>
+					<Text look="heading">About Parable</Text>
+
+					<!-- <Text look="body">Do more with AI</Text> -->
+				</Stack>
+
+				<Stack
+					align="center"
+					alignV="center"
+					css={{
+						flex: 1,
+					}}
+				>
+					<Stack
+						gap="lg"
+						css={{
+							p: '$12',
+						}}
+					>
+						<Text look="heading">What does Thing Forge do?</Text>
+						<Text look="body"
+							>Task Forge is a universal AI interface for makers. Unlike
+							single-model AI interfaces, Parable connects to any public AI
+							model or work environment, enabling users to bring the familiarity
+							and skill they've developed on their Parable interface to any
+							model, flattening the learning curve and maximizing their
+							productivity.</Text
+						>
+					</Stack>
+				</Stack>
+			</Inline>
+		</Content>
+	</Box>
+
+	<!-- 3 -->
+
+	<Box
+		css={{
+			background: `linear-gradient(
+				to right,
+				$grey-50 0%,
+				$grey-50 48%,
+				$grey-950 52%,
+				$grey-950 100%
+			)`,
+		}}
+	>
+		<Content>
+			<Inline
+				css={{
+					minHeight: '100vh',
+					'> div': {
+						flex: 1,
+					},
+				}}
+			>
+				<Stack
+					align="center"
+					alignV="center"
+					css={{
+						flex: 1,
+					}}
+				>
+					<Text look="heading">Parable News</Text>
+
+					<Text look="body">Contact Parable</Text>
+				</Stack>
+
+				<Stack
+					align="center"
+					alignV="center"
+					css={{
+						color: '$grey-50',
+						flex: 1,
+					}}
+				>
+					<Text look="heading">Alpha Details</Text>
+
+					<Text look="body">Register for the Alpha</Text>
+				</Stack>
+			</Inline>
+		</Content>
+	</Box>
+
+	<!--  -->
+</Stack>
