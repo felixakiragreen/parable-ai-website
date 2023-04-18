90bed20cf3717b8518afb91e232bed6293d4ee50
Felix Green <work@felix.green>
2023-04-18 18:23:03 -0400

    add Socials component

---
+++ src/lib/cells/Socials.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { socials as allSocials } from '@/lib/datum/socials'
+	import IconSocialRow from '../bonds/IconSocialRow.svelte'
+
+	export let space = 'sm'
+	export let align = null
+	export let size = 'md'
+	export let css = null
+
+	export let filter: string[] = null
+
+	$: socials = filter
+		? allSocials.filter((social) => filter.includes(social.title))
+		: allSocials
+
+	// variants
+	export let symbol = null
+	export let ground = null
+	export let hoverSymbol = null
+	export let hoverGround = null
+</script>
+
+<IconSocialRow
+	{socials}
+	{css}
+	{space}
+	{align}
+	{size}
+	{...{ symbol, ground, hoverSymbol, hoverGround }}
+/>
