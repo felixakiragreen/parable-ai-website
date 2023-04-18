3c7c6f1df78935d9b9a8d73a6bcbcce26f17516e
Felix Green <work@felix.green>
2023-04-18 17:33:45 -0400

    style the header a bit more

--- src/lib/cells/Header.svelte
+++ src/lib/cells/Header.svelte
@@ -5,27 +5,42 @@
 	import { Content, Inline, Text, Link, HeaderLink } from '@/lib/bonds'
 
 	const ss = stitch({
-		position: 'relative',
+		position: 'absolute',
 		// '@md': {
 		// 	position: 'sticky',
 		// },
-		top: '$0',
-		bg: '$grey-500',
+		top: 0,
+		width: '100%',
+		// bg: '$grey-500',
+		bg: 'hsla(0, 0%, 100%, 0.12)',
 		color: '$foreground',
 		zIndex: 10,
+
+		// background: '$background_50',
+		// color: '$foreground',
+		backdropFilter: 'blur(20px)',
 	})
 </script>
 
 <header class={ss()}>
 	<Content
 		css={{
-			bg: '$grey-500',
-			p: '$4',
+			// bg: '$grey-500',
+			p: '$8',
+			// opacity: '0.5',
 		}}
 	>
+		<div class="bg" />
 		<Inline align="between">
-			<Text>Parable</Text>
-			<Text>Do more with AI</Text>
+			<Inline>
+				<Text look="heading">Parable</Text>
+				<Text>Do more with AI</Text>
+			</Inline>
+			<Inline gap="md">
+				<HeaderLink url="/#why">Why</HeaderLink>
+				<HeaderLink url="/#what">What</HeaderLink>
+				<HeaderLink url="/#who">Who</HeaderLink>
+			</Inline>
 		</Inline>
 	</Content>
 </header>
