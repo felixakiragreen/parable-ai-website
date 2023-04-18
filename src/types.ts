196f05e4695211a25d631831b7bd74e6dc95c84a
Felix Green <work@felix.green>
2023-04-18 18:21:57 -0400

    add social link to types

--- src/types.ts
+++ src/types.ts
@@ -10,4 +10,21 @@ export const baseColors = [
 	'purple',
 	'grey',
 ] as const
-export type Color = typeof baseColors[number]
+export type Color = (typeof baseColors)[number]
+
+export type SocialLink = {
+	url: string
+	username: string
+	title: string
+	hex: string
+	hue: string
+	path: string
+	transform: {
+		scale: number
+		translate: {
+			x: number
+			y: number
+		}
+	}
+	look?: string
+}
